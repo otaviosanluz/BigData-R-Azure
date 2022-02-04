d5ec81475e506909d0db2373c1b5dbc5b4f0dfa9
otaviosanluz <otaviosanluz@gmail.com>
2022-02-04 16:28:57 -0300

    primeiras atividades

---
+++ cap02-R_fundamentos/01-RStudio.R
@@ -0,0 +1,21 @@
+# Primeiros Passos na Linguagem R
+
+# Configurando o diretório de trabalho
+# Coloque entre aspas o diretório de trabakho que você está usando
+setwd("C:/Users/otavio/Mundo/Github/BigData-R-Azure/cap02-R_fundamentos")
+getwd()
+
+# Nome dos contributors
+contributors()
+
+# Licensa
+license()
+
+# Informação sobre a sessão
+sessionInfo()
+
+# Imprimir na tela
+print('Estou iniciando minha caminhada na carreira de Cientista de Dados')
+
+# Criar gráficos
+plot(1:25)
